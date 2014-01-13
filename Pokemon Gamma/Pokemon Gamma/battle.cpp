2068b788be741d0182a50a64e0fdba68633c2400
Yoshin <yoshin@live.fr>
2014-01-13 14:55:49 +0100

    1st Battle_core commiy
    Creating battle core

--- Pokemon Gamma/Pokemon Gamma/battle.cpp
+++ Pokemon Gamma/Pokemon Gamma/battle.cpp
@@ -6,3 +6,37 @@
 //														Battle_Core																		//
 //																																		//
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace BattleCore
+{
+
+	class Battle
+	{
+
+	public:
+		Battle::Battle(Dresseur & myDresseur, const unsigned int & idNpc)
+		{
+			dresseur = myDresseur;
+			npc = idNpc;
+
+			id = 0;
+		}
+
+		void Battle::newBattle()
+		{
+			id = getId();
+		}
+
+		unsigned int getId()
+		{
+
+		}
+
+	protected:
+		unsigned int id;				// ID du Combat
+		Dresseur dresseur;	         	// Code "caractère" du Pokémon
+		unsigned int npc;				// ID du npc
+
+	};
+
+}
--- Pokemon Gamma/Pokemon Gamma/battle.h
+++ Pokemon Gamma/Pokemon Gamma/battle.h
@@ -10,12 +10,6 @@
 #ifndef BATTLE_H
 #define BATTLE_H
 
-enum CourbeXP { Rapide, Normale, Lente, Parabolique, Erratic, Fluctuante, TresRapide, TresLente };
 
-int squared(int a);														//Return a*a
-
-int cube(int a);														//Return a*a*a
-
-unsigned int getNextExp(CourbeXP c, unsigned int level);				//Return the amount of experience need for next level
 
 #endif
